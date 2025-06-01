9623eff622b424a05f4db5f01932159c150141d5
vivek Koyani <vivek.koyani@gmail.com>
2025-06-01 12:21:37 +0530

    add in home page GenderSectionCollections

--- forntend/src/pages/Home.jsx
+++ forntend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/Layout/Hero'
+import GenderSectionCollections from '../components/Products/GenderSectionCollections'
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <GenderSectionCollections />
     </div>
   )
 }
