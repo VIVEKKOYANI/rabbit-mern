0726f176947ff9f85383dd852867d97abb8310cb
vivek Koyani <vivek.koyani@gmail.com>
2025-06-06 10:00:27 +0530

    NewArrivals

--- forntend/src/pages/Home.jsx
+++ forntend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Layout/Hero'
 import GenderSectionCollections from '../components/Products/GenderSectionCollections'
+import NewArrivals from '../components/Products/NewArrivals'
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <GenderSectionCollections />
+      <NewArrivals />
     </div>
   )
 }
